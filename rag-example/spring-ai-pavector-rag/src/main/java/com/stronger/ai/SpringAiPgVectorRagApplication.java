fe730e2dbcb53702d1b174e144e6e6ad7c65bdf9
w <112393402+strongerwe@users.noreply.github.com>
2025-09-06 20:14:43 +0800

    pg-rag

---
+++ rag-example/spring-ai-pavector-rag/src/main/java/com/stronger/ai/SpringAiPgVectorRagApplication.java
@@ -0,0 +1,22 @@
+package com.stronger.ai;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiang.w
+ * @version release-1.0.0
+ * @class SpringAiPgVectorRagApplication.class
+ * @department Platform R&D
+ * @date 2025/9/6
+ * @desc 启动类
+ */
+@SpringBootApplication
+public class SpringAiPgVectorRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiPgVectorRagApplication.class, args);
+    }
+
+}
